9f7881c44e0dbc0f102a730c8089ebf8d6bb6bf6
Knut Melvær <knut.melvaer@gmail.com>
2014-09-11 11:21:06 +0200

    added proxy file

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
 $(document).foundation();
+$.ajax({
+	type: "GET",
+	url: "proxy.php",
+	dataType: "xml",
+	success: viewxml
+});
+function parseXml(xml)
+{
+  //find every Tutorial and print the author
+  $(xml).find("entry").each(function()
+  {
+    $(".feed").append($(this).attr("title") + "<br />");
+  });
 
+  // Output:
+  // The Reddest
+  // The Hairiest
+  // The Tallest
+  // The Fattest
+}
+function viewxml(xml) {
+	console.log(xml);
+	var json = $.xml2json(xml);
+	console.log(json);
+	$.each(json.feed, function(i, item) {
+		console.log(json.feed[i].entry);
+	})
+
+}
--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,1 +1,1 @@
-$(document).foundation();
+function parseXml(e){$(e).find("entry").each(function(){$(".feed").append($(this).attr("title")+"<br />")})}function viewxml(e){console.log(e);var n=$.xml2json(e);console.log(n),$.each(n.feed,function(e,o){console.log(n.feed[e].entry)})}$(document).foundation(),$.ajax({type:"GET",url:"proxy.php",dataType:"xml",success:viewxml});
