76c215e95c1ff8ff514b8b5cb120349b70d090f5
Knut Melvær <knut.melvaer@gmail.com>
2014-10-20 15:45:39 +0200

    Added raw data. Simpler code

--- js/app.js
+++ js/app.js
@@ -1,35 +1,31 @@
 $(document).ready(function() {
-    function search(){
-        var searchstring = $("#search").val();
-
-        $.ajax({
-            url: 'js/content_proxy.php',
-            data: "searchword="+searchstring,
-            // {
-            // searchword: searchstring
-            // itemsPerPage: 2,
-            // filter: "Bøker",
-            // sort: "ASC",
-            // freetext: "true",
-            // facet: "year"
-            //},
-            datatype: 'json',
-            type: 'get',
-            success: function(output) {
-                var ctx = document.getElementById("myChart").getContext("2d");
-                var myLineChart = new Chart(ctx).Line(output, options);
-                var options = {
-
-                };
-            }
-        });
-    }
-    $("#button").click(function(){
+    $( "#search" ).focus();
+    $("form").submit(function(e){
+        e.preventDefault();
         search();
     });
-    $('#search').keyup(function(e) {
-        if(e.keyCode == 13) {
-            search();
-        }
+});
+function search(){
+    var searchstring = $.trim($("#search").val()).replace(' ', '+');
+    $.ajax({
+        url: 'js/content_proxy.php',
+        data: "searchstring="+searchstring,
+        datatype: 'json',
+        type: 'get'
+    })
+    .done(function(output) {
+        var ctx = document.getElementById("myChart").getContext("2d");
+            var myLineChart = new Chart(ctx).Line(output, options);
+            var options = {
+            };
+
+            var csvlist = output.metadata.yearlabels;
+            var csvitems = ["year, ","frequency","<br />"];
+
+            $.each(csvlist, function(i, item) {
+                 csvitems.push(item + ", " + output.metadata.yearcount[i]+"<br />");
+                 // console.log(metadata.yearlabels[i] + ":" + metadata.yearcount[i]);
+             });
+            $("#results").append().html(csvitems);
     });
- });
+}
--- js/content_proxy.php
+++ js/content_proxy.php
@@ -1,50 +1,63 @@
 <?php
-if(isset($_GET['searchword']) && !empty($_GET['searchword'])) {
-header('Content-type: application/json');
-$searchword = $_GET['searchword'];
-$searchbaseurl = "http://www.nb.no/services/search/v2/search";
-//$url = $searchbaseurl."?q=".$searchword;
-// $url = 'http://www.nb.no/services/search/v2/search/?q='.$searchword.'&facet=year&itemsPerPage=10';
-$url = 'http://www.nb.no/services/search/v2/search/?q='.urlencode($searchword).'&facet=year&itemsPerPage=10';
+if(isset($_GET['searchstring']) && !empty($_GET['searchstring'])) {
+    header('Content-type: application/json');
+    $searchstring = $_GET['searchstring'];
+    $searchbaseurl = "http://www.nb.no/services/search/v2/search?q=";
+    $url = $searchbaseurl.$searchstring."&(contentClasses:'restricted'%20OR%20contentClasses:'public'%20OR%20contentClasses:'bokhylla'%20OR%20metadataClasses:'public'%20OR%20metadataClasses:'restricted')&ft=true&facet=year&itemsPerPage=1";
+    // $searchparameters = "%20(contentClasses:%22public%22)&ft=all";
+    // $url = $searchbaseurl."?q=".$searchstring."%20".$searchparameters;
+    // $url = "http://www.nb.no/services/search/v2/search?q=hamsun%20facet=year%20itemsPerPage=1"
+    // $url = 'http://www.nb.no/services/search/v2/search/?q='.$searchstring.'&facet=year&itemsPerPage=10';
+    // $str = "?q=".urlencode($searchstring)."%20(contentClasses:%22public%22)&ft=".$freetext."&itemsPerPage=".$itemsPerPage."&filter=".urlencode($filter)."&sort=".$sort;
+    //$url = 'http://www.nb.no/services/search/v2/search/?q='.urlencode($searchstring).'&facet=year&itemsPerPage=10';
+    $data = file_get_contents($url);
 
-$data = file_get_contents($url);
+    $data_split = explode("<nb:values>", $data);
+    $data_split2 = explode("</nb:values>", $data_split[1]);
+    $data_splitted = $data_split2[0];
+    $data_lines = explode("</nb:value>", $data_splitted);
 
-$data_split = explode("<nb:values>", $data);
-$data_split2 = explode("</nb:values>", $data_split[1]);
-$data_splitted = $data_split2[0];
-$data_lines = explode("</nb:value>", $data_splitted);
-
-$dataArray = array();
-foreach($data_lines as $line) {
-    if (strlen($line) > 1)  { // to avoid the last line, which is empty
-        $matches = array();
-        $t = preg_match('/"(.*?)"/s', $line, $matches);
-        $count = intval($matches[1]);
-        $year = substr($line, -4);
-        $dataArray[$year] = $count;
+    $dataArray = array();
+    foreach($data_lines as $line) {
+        if (strlen($line) > 1)  { // to avoid the last line, which is empty
+            $matches = array();
+            $t = preg_match('/"(.*?)"/s', $line, $matches);
+            $count = intval($matches[1]);
+            $year = substr($line, -4);
+            $dataArray[$year] = $count;
+        }
     }
-}
 
-ksort($dataArray); // sort data by year (i.e. sort array by key)
+    ksort($dataArray); // sort data by year (i.e. sort array by key)
 
-$json_data = array();
-$json_labels = array();
-foreach ($dataArray as $year => $count) {
-    $json_data[] = $count;
-    $json_labels[] = $year;
-}
+    $json_data = array();
+    $json_labels = array();
+    foreach ($dataArray as $year => $count) {
+        $json_data[] = $count;
+        $json_labels[] = $year;
+    }
 
-echo json_encode(
-        array("labels" => $json_labels,
-        "datasets" => array(array(
-            "label" => "NB",
-            "fillColor" => "rgba(220,220,220,0.6)",
-            "strokeColor" => "rgba(213, 2, 7, 1)",
-            "pointColor" => "rgba(213, 2, 7, 0.7)",
-            "pointStrokeColor" => "#fff",
-            "pointHighlightFill" => "rgba(213, 2, 7, 0.4)",
-            "pointHighlightStroke" => "rgba(220,220,220,1)",
-            "data" => $json_data)))
-    );
+    echo json_encode(
+            array("labels" => $json_labels,
+                "datasets" =>
+                    array(
+                        array(
+                            "label" => "NB",
+                            "fillColor" => "rgba(220,220,220,0.6)",
+                            "strokeColor" => "rgba(213, 2, 7, 1)",
+                            "pointColor" => "rgba(213, 2, 7, 0.7)",
+                            "pointStrokeColor" => "#fff",
+                            "pointHighlightFill" => "rgba(213, 2, 7, 0.4)",
+                            "pointHighlightStroke" => "rgba(220,220,220,1)",
+                            "data" => $json_data
+                        )
+                    ),
+                    "metadata" => array(
+                        "yearlabels" => $json_labels,
+                        "yearcount" => $json_data,
+                        "seachstring" => $searchstring
+                    )
+            )
+        );
 }
 ?>
